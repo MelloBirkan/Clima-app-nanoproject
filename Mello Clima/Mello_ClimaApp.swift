8b5a7a8fd827a13de95804628e77eb35d187583d
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-06-08 15:46:28 -0300

    Initial Commit

---
+++ Mello Clima/Mello_ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mello_ClimaApp.swift
+//  Mello Clima
+//
+//  Created by Marcello Gonzatto Birkan on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Mello_ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
